a07368eba6d135a787f29bfb66f688aa86f8ce3b
johngerome <johngerome@gmail.com>
2013-09-18 06:08:43 +0800

    clean up

--- data/logs/errors/exceptions/20130514.log.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Forbidden'); ?>
-[2013-05-14 10:00:37] (Flux_Error) Exception Flux_Error: MySQL error (SQLSTATE: 42S02, ERROR: 1146): Table 'ragnarok_ra.mob_db2' doesn't exist
-[2013-05-14 10:00:37] (Flux_Error) **TRACE** #0 D:\Xampp\htdocs\RO\gerome-fluxcp\lib\Flux\Template.php(358): include()
-[2013-05-14 10:00:37] (Flux_Error) **TRACE** #1 D:\Xampp\htdocs\RO\gerome-fluxcp\lib\Flux\Dispatcher.php(177): Flux_Template->render()
-[2013-05-14 10:00:37] (Flux_Error) **TRACE** #2 D:\Xampp\htdocs\RO\gerome-fluxcp\index.php(177): Flux_Dispatcher->dispatch(Array)
-[2013-05-14 10:00:37] (Flux_Error) **TRACE** #3 {main}
--- data/logs/mysql/errors/20130514.log.php
+++
@@ -1,2 +0,0 @@
-<?php exit('Forbidden'); ?>
-[2013-05-14 10:00:37] [SQLSTATE=42S02] Err 1146: Table 'ragnarok_ra.mob_db2' doesn't exist
